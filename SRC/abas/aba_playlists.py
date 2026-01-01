71731f7cec409aaec811a27a8687d69c8508dc82
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2026-01-01 18:20:00 -0300

    Acrescentando faixas relacionadas à cada playlist.

--- SRC/abas/aba_playlists.py
+++ SRC/abas/aba_playlists.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from database import listar_playlists
+from database import listar_playlists, listar_faixas_da_playlist
 
 class AbaPlaylists(ctk.CTkFrame):
     def __init__(self, parent):
@@ -11,7 +11,7 @@ class AbaPlaylists(ctk.CTkFrame):
         self.btn = ctk.CTkButton(self, text="Atualizar Lista", command=self.listar)
         self.btn.pack(pady=10)
 
-        self.lista = ctk.CTkTextbox(self, width=400, height=200)
+        self.lista = ctk.CTkTextbox(self, width=450, height=200)
         self.lista.pack(pady=10)
 
     def listar(self):
@@ -23,4 +23,16 @@ class AbaPlaylists(ctk.CTkFrame):
             nome = playlist['nome']
             data = playlist['data_criacao']
             
-            self.lista.insert("end", f"ID: {cod} | {nome} | {data})\n")
+            self.lista.insert("end", f"ID: {cod} | NOME: {nome} | CRIAÇÃO: {data}\n")
+            
+            faixas = listar_faixas_da_playlist(cod)
+            
+            if not faixas:
+                self.lista.insert("end", "\t(Nenhuma faixa cadastrada)\n")
+            else:
+                for faixa in faixas:
+                    f_num = faixa['num_faixa']
+                    f_desc = faixa['descricao']
+                    f_disc = faixa['num_disco']
+                    
+                    self.lista.insert("end", f"\tNUM: {f_num} | {f_desc} (DISCO: {f_disc})\n")
