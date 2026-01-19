625e48fd78abea94b9ee177049fdd2462a8abf1e
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2026-01-19 07:56:34 -0300

    Conserta view.

--- SQL/05 - view.sql
+++ SQL/05 - view.sql
@@ -1,13 +1,14 @@
-USE BDSpotPer
+USE BDSpotPer;
 GO
 
-CREATE VIEW VW_Playlist_Qtd_Albuns
+CREATE VIEW dbo.VW_Playlist_Album
 WITH SCHEMABINDING
 AS
-SELECT 
+SELECT
     p.cod_play,
     p.nome AS nome_playlist,
-    COUNT(DISTINCT f.album) AS qtd_albuns
+    f.album,
+    COUNT_BIG(*) as count
 FROM dbo.Playlist p
 JOIN dbo.Faixa_Playlist fp
     ON fp.cod_play = p.cod_play
@@ -15,4 +16,24 @@ JOIN dbo.Faixa f
     ON f.num_faixa = fp.num_faixa
    AND f.album     = fp.album
    AND f.num_disco = fp.num_disco
-GROUP BY p.cod_play, p.nome;
+GROUP BY
+    p.cod_play,
+    p.nome,
+    f.album;
+GO
+
+CREATE UNIQUE CLUSTERED INDEX IX_VW_Playlist_Album
+ON dbo.VW_Playlist_Album (cod_play, album);
+GO
+
+---
+
+CREATE VIEW dbo.VW_Playlist_Qtd_Albuns
+AS
+SELECT
+    cod_play,
+    nome_playlist,
+    COUNT(*) AS qtd_albuns
+FROM dbo.VW_Playlist_Album
+GROUP BY cod_play, nome_playlist;
+GO
